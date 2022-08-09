dec1be84a903c18fe427ec1b3896efa1da72d254
NaganoharaJ <250502888@qq.com>
2022-08-09 22:45:41 +0800

    third commit

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,21 @@
 package controllers
+
+func CreateBook() {
+
+}
+
+func GetBooks() {
+
+}
+
+func GetBookByID() {
+
+}
+
+func UpdateBook() {
+
+}
+
+func DeleteBook() {
+
+}
--- pkg/routes/bookstore-routes.go
+++ pkg/routes/bookstore-routes.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/NaganoharaJ/go-bookstore/pkg/controllers"
+	"github.com/gorilla/mux"
+)
+
+var RegisterBookStoreRoutes = func(router *mux.Router) {
+	router.HandleFunc("/book/", controllers.CreateBook)
+}
